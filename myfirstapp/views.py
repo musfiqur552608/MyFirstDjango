e752348a238e092e3558db85dd89da9b390d469b
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-01-07 06:03:29 +0600

    Working on html file

---
+++ myfirstapp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+def myfunctioncall(request):
+    return HttpResponse("Hello World")
+
+def myfunctionabout(request):
+    return HttpResponse("About Response")
+
+def add(request, a, b):
+    return HttpResponse(a+b)
+
+def intro(request, name, age):
+    mydictionary = {
+        "name" : name,
+        "age" : age
+    }
+    return JsonResponse(mydictionary)
+
+def myfirstpage(request):
+    return render(request, 'index.html')
+
+def mysecondpage(request):
+    return render(request, 'second.html')
+
+def mythirdpage(request):
+    var = "hello world"
+    greeting = "hey how are you"
+    fruits = ["apple", "mango", "banana"]
+    num1, num2 = 3, 5
+    ans = num1 > num2
+    #print(ans)
+    mydictionary = {
+        "var" : var,
+        "msg" : greeting,
+        "myfruits" : fruits,
+        "num1" : num1,
+        "num2" : num2,
+        "ans" : ans
+    }
+    return render(request, 'third.html', context=mydictionary)
+
+def myimagepage(request):
+    return render(request, 'imagepage.html')
+
+def myimagepage2(request):
+    return render(request, 'imagepage2.html')
